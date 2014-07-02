1f3912e5b025577c82f2bec35035e7dbd3547d24
4ybakut2004 <4ybakut2004@gmil.com>
2014-07-02 21:24:09 +0700

    Первый коммит

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NoragamiAdverts::Application.config.secret_key_base = 'ea1b98302d1411cfa2ed145c47503af68472246c3f4bea4a90a94f8ad011526768cf90885c0974f809ca3886352299473c1e4cb946464dfb7e61282b6801149b'

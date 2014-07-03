4557730b812aae301367f26027baa9a7fe845c31
4ybakut2004 <4ybakut2004@gmil.com>
2014-07-03 13:03:42 +0700

    На локалке заработало

--- app/assets/javascripts/three.js
+++ app/assets/javascripts/three.js
@@ -11956,8 +11956,9 @@ THREE.JSONLoader.prototype.loadAjaxJSON = function ( context, url, callback, tex
 			if ( xhr.status === 200 || xhr.status === 0 ) {
 
 				if ( xhr.responseText ) {
+					//console.log(xhr.responseText.replace(/;/, ""));
 
-					var json = JSON.parse( xhr.responseText );
+					var json = JSON.parse( xhr.responseText.replace(/;\s*$/, "") );
 
 					if ( json.metadata !== undefined && json.metadata.type === 'scene' ) {
 
